4c5153b24f14b04eaf1905c75028a9c461db5e75
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-09 19:10:27 +0200

    style: use pycodestyle style

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
+"""
+This module contains a type annotated function add that takes two floats
+"""
 
-def add (a: float, b: float) -> float:
+
+
+def add(a: float, b: float) -> float:
     """
     Adds two floating-point numbers.
 
     Parameters:
-        a (float): The first number to add.
-        b (float): The second number to add.
+    a (float): The first number to add.
+    b (float): The second number to add.
     
     Returns:
-        float: The sum of the two numbers.
+    float: The sum of the two numbers.
     """
     return a + b;
