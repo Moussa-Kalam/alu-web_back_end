85c64d90a5d71a5074db9effcb911ae2e8bf108c
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-09 18:06:22 +0200

    feat: basic notation on a function that add two floats

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add (a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+
+    Parameters:
+        a (float): The first number to add.
+        b (float): The second number to add.
+    
+    Returns:
+        float: The sum of the two numbers.
+    """
+    return a + b;
---
+++ python_variable_annotations/README.md
@@ -0,0 +1,12 @@
+# Python - Variable Annotations
+
+## Learning objectives
+
+- Type annotations in Python 3
+- How to use type annotations to specify function signatures and variable types
+- Duck typing
+- How to validate you code with `mypy`
+
+## Requirements
+
+- The code should use the `pycodestyle` style
