805efe71552c034f3e5ba4f4bef95bc0ad2be404
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-12 01:50:37 +0200

    refactor: fix pycodestyle validation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -7,7 +7,8 @@
 
 def add(a: float, b: float) -> float:
     """
-        A type-annotated function add that takes a float a and a float b as arguments
+        A type-annotated function add that takes
+        a float a and a float b as arguments
         and returns their sum as a float
     """
     return a + b
