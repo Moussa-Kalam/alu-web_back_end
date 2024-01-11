fa7db4309c3f334e08feadb0ca7a42ab6af64b4e
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-12 01:58:00 +0200

    feat: annotate a function that return the floor of a float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    A function that returns the floor value of a float
+    """
+    return int(n)
