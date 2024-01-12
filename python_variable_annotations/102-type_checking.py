4330f1298780169b78489359bd036c0715bb5498
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-12 15:47:33 +0200

    feat: code validation with mypy and annotation

---
+++ python_variable_annotations/102-type_checking.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Type Checking
+"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Code Validation using mypy
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for _ in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
