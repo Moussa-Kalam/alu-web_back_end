c0f7de0f457f73cbf056175ef797be6aad43f7e8
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-12 15:26:40 +0200

    refactor: fix pycodestyle validation "blank line at end of file"

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -6,4 +6,3 @@ from typing import Sequence, Iterable, List, Tuple
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ Element length """
     return [(i, len(i)) for i in lst]
-
