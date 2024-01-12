99d15a1725438ecb96fb6fa49218384f57a7389a
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-12 02:01:07 +0200

    feat: annotate a function that returns the string representation of a float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    A function that returns the string representation of a float
+    """
+    return str(n)
