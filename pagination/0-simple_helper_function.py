8b994a949fefb50313368f3d78fb4a22a890fb7e
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-19 13:07:14 +0200

    refactor: pycodestyle validation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,7 +8,7 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     return a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes 
+    corresponding to the range of indexes
     """
 
-    return (page * page_size - page_size, page * page_size)
+    return page * page_size - page_size, page * page_size
