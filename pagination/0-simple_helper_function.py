22a6fbdebbe22c40cfa9da6330384813f8ee65d4
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-19 12:56:32 +0200

    feat: simple helper function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Simple helper function"""
+
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes 
+    """
+
+    return (page * page_size - page_size, page * page_size)
