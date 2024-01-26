7db3e1f228a069e9768602fe5dbd52fe1efbfb21
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-26 20:56:28 +0200

    feat: connect to secure database

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,7 +6,8 @@ Filter datum logger
 import re
 from typing import List
 import logging
-
+import os
+import mysql.connector
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
@@ -33,6 +34,20 @@ def get_logger() -> logging.Logger:
     handler.setFormatter(RedactingFormatter(PII_FIELDS))
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """connect to the database"""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
+    host = os.getenv("PERSONAL_DATA_DB_HOST")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=db_name)
 
 
 class RedactingFormatter(logging.Formatter):
