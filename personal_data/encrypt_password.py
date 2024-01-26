91c6d52694ba14bc1f9d59de09fea55fe6965553
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-26 21:14:43 +0200

    feat: check valid password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ encrypt password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check if password is valid """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
