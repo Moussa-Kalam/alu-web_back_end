d89712debf79b80bcf1730b8c9a05ae409c6da87
Moussa-Kalam <m.amzat@alustudent.com>
2024-01-26 20:24:26 +0200

    feat: create logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,6 +8,9 @@ from typing import List
 import logging
 
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
 def filter_datum(fields: List[str],
                  redaction: str,
                  message: str,
@@ -19,6 +22,17 @@ def filter_datum(fields: List[str],
         message = re.sub(field + "=.*?" + separator,
                          field + "=" + redaction + separator, message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """return logger"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
