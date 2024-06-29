e734ad839146c7340c80f96dcdac892348b82b1f
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-06-29 15:36:47 +0200

    feat: create a basic flask app

---
+++ i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    """Home page"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
