729b57f350cbfbaca0b2911bf9fa75886a7cd42b
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-06-29 16:11:19 +0200

    feat: parametrize templates

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Basic Flask app that implements i18n and internationalization"""
+""" Basic Flask app that implements i18n and internationalization """
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
