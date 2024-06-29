32e9df41aff03cf76b9ec3c65b26c16551dc3622
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-06-29 23:55:30 +0200

    feat: store lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -18,6 +18,21 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """ Save the call history of different function calls """
+
+    @functools.wraps(method)
+    def wrapper(self, *args):
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args)
+        self._redis.rpush(output_key, output)
+        return output
+
+    return wrapper
+
+
 class Cache:
     """ Redis cache """
 
@@ -27,6 +42,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Generate random key and store data in redis """
         key: str = str(uuid.uuid4())
@@ -48,4 +64,3 @@ class Cache:
     def get_int(self, key: str) -> int:
         """ Get data from redis and transform it to int """
         return self.get(key, int)
-
