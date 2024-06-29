137b858abc710539acd6bd315daa8153f1b7bafc
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-06-29 15:56:55 +0200

    feat: get locale from request

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Basic Flask app"""
+""" Basic Flask app """
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
@@ -9,7 +9,7 @@ babel = Babel(app)
 
 
 class Config:
-    """Config class"""
+    """ Config class """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,13 +20,13 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """Get locale"""
+    """ Get locale """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
-    """Home page"""
+    """ Home page """
     return render_template('2-index.html')
 
 
