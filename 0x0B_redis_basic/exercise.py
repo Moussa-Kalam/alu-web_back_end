c35be6798613ac96aceae40b6390858a833fa4a4
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-06-29 23:29:47 +0200

    feat: read from Redis and recover original type

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -18,3 +18,18 @@ class Cache:
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: callable = None):
+        """ Get data from redis and optionally transform it using fn """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ Get data from redis and transform it to string """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """ Get data from redis and transform it to int """
+        return self.get(key, int)
