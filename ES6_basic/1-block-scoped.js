d904d6075b078a817d453740f745cfa0690054c0
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-12 21:46:14 +0200

    fix: change variables to let

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
     const task = true;
