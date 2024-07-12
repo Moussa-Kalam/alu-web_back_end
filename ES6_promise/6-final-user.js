6b7830a341852a8e5ebd99d0a6e27e4484a8ff7d
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-12 23:36:14 +0200

    remove trailing commas

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,20 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(
-  firstName,
-  lastName,
-  filename
-) {
-  const data = await Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(filename)
-  ]);
-  return data.map((item) => ({
-    status: item.status,
-    value:
-      item.status === 'fulfilled'
-        ? item.value
-        : `${item.reason.name}: ${item.reason.message}`,
-  }));
+export default async function handleProfileSignup(firstName, lastName, filename) {
+  const data = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)]);
+  return data
+    .map((item) => ({
+      status: item.status,
+      value: item.status === 'fulfilled' ? item.value : `${item.reason.name}: ${item.reason.message}`,
+    }));
 }
