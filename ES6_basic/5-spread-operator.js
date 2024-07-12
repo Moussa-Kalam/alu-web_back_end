02f9cb3da2551642051f31b20c8382cfddddcf7e
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-12 19:41:12 +0200

    refactor: update parameter

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,3 +1,3 @@
-export default function concatArrays(array1, array2, array3) {
-  return [...array1, ...array2, ...array3];
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
 }
