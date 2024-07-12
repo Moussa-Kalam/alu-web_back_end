2fb0601f4742e89733317678209213d64e8242b5
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-12 23:20:58 +0200

    Handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  filename
+) {
+  const data = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename),
+  ]);
+  return data.map((item) => ({
+    status: item.status,
+    value:
+      item.status === 'fulfilled'
+        ? item.value
+        : `${item.reason.name}: ${item.reason.message}`,
+  }));
+}
