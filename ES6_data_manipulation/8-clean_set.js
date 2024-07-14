580af030a666b2bdc412fc544c54bbb5f7fa9ce7
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-14 14:22:00 +0200

    feat: clean set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,17 @@
+export default function cleanSet(set, startString) {
+  if (
+    !set ||
+    !startString ||
+    typeof set !== 'object' ||
+    typeof startString !== 'string'
+  )
+    return '';
+
+  return [...set]
+    .filter(
+      (element) =>
+        typeof element === 'string' && element.startsWith(startString)
+    )
+    .map((element) => element.replace(startString, ''))
+    .join('-');
+}
