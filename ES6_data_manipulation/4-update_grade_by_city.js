d6699c08421df75ca2ca6dfb67b7d5f6598f4207
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-14 13:58:34 +0200

    feat: update grade by city

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grade = newGrades.find(
+        (newGrade) => newGrade.studentId === student.id,
+      );
+      return { ...student, grade: grade ? grade.grade : 'N/A' };
+    });
+}
