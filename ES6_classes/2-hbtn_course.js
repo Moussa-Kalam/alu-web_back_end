12dcb51fa7bee76bca4589d47d226d36be79cb7f
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-14 12:15:41 +0200

    refactor: fix constructor types check and lint error

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -18,13 +18,16 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
-    if (typeof newName !== 'string') throw TypeError('Name must be a string');
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
     this._name = newName;
   }
 
   set length(newLength) {
-    if (typeof newLength !== 'number')
+    if (typeof newLength !== 'number') {
       throw TypeError('Length must be a number');
+    }
     this._length = newLength;
   }
 
@@ -32,8 +35,9 @@ export default class HolbertonCourse {
     if (
       !Array.isArray(newStudents) ||
       newStudents.some((student) => typeof student !== 'string')
-    )
+    ) {
       throw TypeError('Students must be an array of string');
+    }
     this._students = newStudents;
   }
 }
