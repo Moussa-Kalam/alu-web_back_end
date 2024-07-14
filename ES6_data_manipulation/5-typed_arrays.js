62023da18953f6571a0bae7dbfdb35b4b3dadae2
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-14 14:04:27 +0200

    feat: typed arrays

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,7 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  if (position > int8Array.length) throw new Error('Position outside range');
+  int8Array[position] = value;
+  return new DataView(buffer);
+}
