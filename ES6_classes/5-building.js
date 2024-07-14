f3e73daf7d9706e811ef15f7c1ba8ed4b56efc30
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-14 13:02:32 +0200

    fix: Missing trailing comma comma-dangle

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,8 +3,7 @@ export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
       throw new Error(
-        'Class extending Building must override evacuationWarningMessage'
-      );
+        'Class extending Building must override evacuationWarningMessage');
     }
 
     this._sqft = sqft;
