e9fb47b30d2fa95945efcc3668e633f224e42bdc
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-14 12:42:55 +0200

    feat: implement Building class

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+/* eslint-disable class-methods-use-this */
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
