3876e3e20e8747b2f62fa52c426e69353a05541e
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-14 14:25:35 +0200

    refactor: correct lint issues

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,15 @@
 export default function cleanSet(set, startString) {
   if (
-    !set ||
-    !startString ||
-    typeof set !== 'object' ||
-    typeof startString !== 'string'
-  )
+    !set 
+    || !startString 
+    || typeof set !== 'object' 
+    || typeof startString !== 'string'
+  ) {
     return '';
+  }
 
   return [...set]
-    .filter(
-      (element) =>
-        typeof element === 'string' && element.startsWith(startString)
-    )
+    .filter((element) => typeof element === 'string' && element.startsWith(startString))
     .map((element) => element.replace(startString, ''))
     .join('-');
 }
