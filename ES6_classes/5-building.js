730e26060cf324539c2a5bb40496f9e119680ec3
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-14 13:04:34 +0200

    fix: lint errors

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,8 +2,7 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage');
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
     this._sqft = sqft;
