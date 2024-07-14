0fc9809e8e3e4dd3162a9684c0b1199053aca7f7
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-07-14 12:01:11 +0200

    feat: a course, getters, setters

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') throw TypeError('Name must be a string');
+    this._name = newName;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number')
+      throw TypeError('Length must be a number');
+    this._length = newLength;
+  }
+
+  set students(newStudents) {
+    if (
+      !Array.isArray(newStudents) ||
+      newStudents.some((student) => typeof student !== 'string')
+    )
+      throw TypeError('Students must be an array of string');
+    this._students = newStudents;
+  }
+}
